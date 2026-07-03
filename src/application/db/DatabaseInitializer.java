09679856dad7bfa940e3810438851c6d86eb598a
Amiel14 <amiel1406@gmail.com>
2026-07-03 09:29:25 +0800

    Changed initialize() to static

--- src/application/db/DatabaseInitializer.java
+++ src/application/db/DatabaseInitializer.java
@@ -6,7 +6,7 @@ import java.sql.Statement;
 
 public class DatabaseInitializer {
 	
-	public void initialize(Connection conn) throws SQLException {
+	public static void initialize(Connection conn) throws SQLException {
 		try (Statement stmt = conn.createStatement()){
 			stmt.execute("PRAGMA foreign_keys = ON");
 			
