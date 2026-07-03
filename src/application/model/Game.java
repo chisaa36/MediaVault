e42fe4832acd645ee7a6480a752cd28532b1bff9
Amiel14 <amiel1406@gmail.com>
2026-07-03 08:49:16 +0800

    Implemented getters and setters

--- src/application/model/Game.java
+++ src/application/model/Game.java
@@ -10,4 +10,17 @@ public class Game {
 	public double userRating;
 	public String developer;
 	public int avgPlaytimeMins;
+	
+	// getters and setters
+	public String getTitle() {return title;}
+	public List<String> getGenres() {return genres;}
+	public double getUserRating() {return userRating;}
+	public String getDeveloper() {return developer;}
+	public int getAvgPlaytimeMins() {return avgPlaytimeMins;}
+	
+	public void setTitle(String title) {this.title = title;}
+	public void setGenres(List<String> genres) {this.genres = genres;}
+	public void setUserRating(double userRating) {this.userRating = userRating;}
+	public void setDeveloper(String developer) {this.developer = developer;}
+	public void setAvgPlaytimeMins(int avgPlaytimeMins) {this.avgPlaytimeMins = avgPlaytimeMins;}
 }
--- src/application/model/Song.java
+++ src/application/model/Song.java
@@ -12,4 +12,21 @@ public class Song {
 	public String artist;
 	public int yearReleased;
 	public int runtimeMins;
+	
+	// getters and setters
+	public String getTitle() {return title;}
+	public List<String> getGenres() {return genres;}
+	public double getUserRating() {return userRating;}
+	public String getAlbum() {return album;}
+	public String getArtist() {return artist;}
+	public int getYearReleased() {return yearReleased;}
+	public int getRuntimeMins() {return runtimeMins;}
+	
+	public void setTitle(String title) {this.title = title;}
+	public void setGenres(List<String> genres) {this.genres = genres;}
+	public void setUserRating(double userRating) {this.userRating = userRating;}
+	public void setAlbum(String album) {this.album = album;}
+	public void setArtist(String artist) {this.artist = artist;}
+	public void setYearReleased(int yearReleased) {this.yearReleased = yearReleased;}
+	public void setRuntimeMins(int runtimeMins) {this.runtimeMins = runtimeMins;}
 }
