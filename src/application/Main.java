f5b198096e0bec4448aa6f2f91866ec85fd84808
Amiel14 <amiel1406@gmail.com>
2026-07-03 13:00:09 +0800

    Tested GamesDAOImpl methods

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,67 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import application.dao.impl.GamesDAOImpl;
+import application.db.DatabaseConnection;
+import application.db.DatabaseInitializer;
+import application.model.Game;
+
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		// establish connection to database and create tables
+		Connection conn = DatabaseConnection.connect();
 		
+		// registers a user and returns its id
+		DatabaseInitializer.initialize(conn);
+		int userId = DatabaseInitializer.registerUser(conn, "Amiel");
+		
+		// instantiate DAO
+		GamesDAOImpl gamesDAOImpl = new GamesDAOImpl(conn, userId);
+		
+		// instantiate a Game to add
+		Game game = new Game("Minecraft", "Completed", 9.0, "Mojang", 60);
+		
+		// must confirm data is added through terminal
+		try {
+			gamesDAOImpl.addGame(game);
+			System.out.println("Game added successfully.");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		// get "Minecraft" game
+		Game output = gamesDAOImpl.getGameByTitle("Minecraft");
+		
+		// print output
+		System.out.println(output.getTitle() + "\t" + output.getStatus() + "\t" + output.getUserRating() + "\t"
+						 + output.getDeveloper() + "\t" + output.getAvgPlaytimeMins());
+		
+		//instantiate and add another game
+		game = new Game("VALORANT", "Completed", -1, "Rito", 999);
+		try {
+			gamesDAOImpl.addGame(game);
+			System.out.println("Game added successfully.");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		// get "2nd" game
+		output = gamesDAOImpl.getGameById(2);
+		System.out.println(output.getTitle() + "\t" + output.getStatus() + "\t" + output.getUserRating() + "\t"
+						 + output.getDeveloper() + "\t" + output.getAvgPlaytimeMins());
+		
+		// return all games
+		System.out.println("### DISPLAY ALL GAMES ###");
+		List<Game> outputs = new ArrayList<Game>();
+		outputs = gamesDAOImpl.getGamesByUser(userId);
+		for (Game entry : outputs) {
+			System.out.println(entry.getTitle() + "\t" + entry.getStatus() + "\t" + entry.getUserRating() + "\t"
+					 + entry.getDeveloper() + "\t" + entry.getAvgPlaytimeMins());
+		}
 	}
 }
