31d8fd9f18f508ff057315fc77eafc6ac29236fa
Amiel14 <amiel1406@gmail.com>
2026-07-03 09:23:06 +0800

    Implemented table-based genre identifier

--- src/application/model/Song.java
+++ src/application/model/Song.java
@@ -1,12 +1,8 @@
 package application.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Song {
 	
 	public String title;
-	public List<String> genres = new ArrayList<>();
 	public double userRating;
 	public String album;
 	public String artist;
@@ -15,7 +11,6 @@ public class Song {
 	
 	// getters and setters
 	public String getTitle() {return title;}
-	public List<String> getGenres() {return genres;}
 	public double getUserRating() {return userRating;}
 	public String getAlbum() {return album;}
 	public String getArtist() {return artist;}
@@ -23,7 +18,6 @@ public class Song {
 	public int getRuntimeMins() {return runtimeMins;}
 	
 	public void setTitle(String title) {this.title = title;}
-	public void setGenres(List<String> genres) {this.genres = genres;}
 	public void setUserRating(double userRating) {this.userRating = userRating;}
 	public void setAlbum(String album) {this.album = album;}
 	public void setArtist(String artist) {this.artist = artist;}
