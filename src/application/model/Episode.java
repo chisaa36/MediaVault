b0b441406668e4202deaf3ed31eec3e7716d455f
Amiel14 <amiel1406@gmail.com>
2026-07-06 19:06:21 +0800

    Added episode class

---
+++ src/application/model/Episode.java
@@ -0,0 +1,20 @@
+package application.model;
+
+public class Episode {
+
+	private String title;
+	private Status status;
+	private double user_rating;
+	private String review;
+	
+	// getters and setters
+	public String getTitle() {return title;}
+	public Status getStatus() {return status;}
+	public double getUser_rating() {return user_rating;}
+	public String getReview() {return review;}
+	
+	public void setTitle(String title) {this.title = title;}
+	public void setStatus(Status status) {this.status = status;}
+	public void setUser_rating(double user_rating) {this.user_rating = user_rating;}
+	public void setReview(String review) {this.review = review;}
+}
