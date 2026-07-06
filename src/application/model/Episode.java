8360ba4c228325e6ecaa5502e23fd0c37322bb6d
Amiel14 <amiel1406@gmail.com>
2026-07-06 22:08:40 +0800

    Fixed field name

--- src/application/model/Episode.java
+++ src/application/model/Episode.java
@@ -4,25 +4,25 @@ public class Episode {
 
 	private String title;
 	private Status status;
-	private double user_rating;
+	private double userRating;
 	private String review;
 	
 	// constructor
 	public Episode(String title, Status status, double user_rating, String review) {
 		this.title = title;
 		this.status = status;
-		this.user_rating = user_rating;
+		this.userRating = user_rating;
 		this.review = review;
 	}
 	
 	// getters and setters
 	public String getTitle() {return title;}
 	public Status getStatus() {return status;}
-	public double getUser_rating() {return user_rating;}
+	public double getUserRating() {return userRating;}
 	public String getReview() {return review;}
 	
 	public void setTitle(String title) {this.title = title;}
 	public void setStatus(Status status) {this.status = status;}
-	public void setUser_rating(double user_rating) {this.user_rating = user_rating;}
+	public void setUserRating(double user_rating) {this.userRating = user_rating;}
 	public void setReview(String review) {this.review = review;}
 }
