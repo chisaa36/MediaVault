50cfd2bfea1433aa9bef6a2bddb9a148615475b2
Amiel14 <amiel1406@gmail.com>
2026-07-06 21:40:30 +0800

    Added constructors, getters, and setters

--- src/application/model/Show.java
+++ src/application/model/Show.java
@@ -9,4 +9,33 @@ public class Show {
 	public int avgMinsPerEp;
 	public int firstYearAired;
 	public int lastYearAired;
+	
+	public Show(String title, double userRating, int numOfSeasons, int numOfEpisodes, int avgMinsPerEp,
+			int firstYearAired, int lastYearAired) {
+		super();
+		this.title = title;
+		this.userRating = userRating;
+		this.numOfSeasons = numOfSeasons;
+		this.numOfEpisodes = numOfEpisodes;
+		this.avgMinsPerEp = avgMinsPerEp;
+		this.firstYearAired = firstYearAired;
+		this.lastYearAired = lastYearAired;
+	}
+	
+	public String getTitle() {return title;}
+	public double getUserRating() {return userRating;}
+	public int getNumOfSeasons() {return numOfSeasons;}
+	public int getNumOfEpisodes() {return numOfEpisodes;}
+	public int getAvgMinsPerEp() {return avgMinsPerEp;}
+	public int getFirstYearAired() {return firstYearAired;}
+	public int getLastYearAired() {return lastYearAired;}
+	
+	public void setTitle(String title) {this.title=title;}
+	public void setUserRating(double userRating) {this.userRating=userRating;}
+	public void setNumOfSeasons(int numOfSeasons) {this.numOfSeasons=numOfSeasons;}
+	public void setNumOfEpisodes(int numOfEpisodes) {this.numOfEpisodes=numOfEpisodes;}
+	public void setAvgMinsPerEp(int avgMinsPerEp) {this.avgMinsPerEp=avgMinsPerEp;}
+	public void setFirstYearAired(int firstYearAired) {this.firstYearAired=firstYearAired;}
+	public void setLastYearAired(int lastYearAired) {this.lastYearAired=lastYearAired;}
+
 }
