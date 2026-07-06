03cb2c5f5e641820f82422992f6cc86ba70c5b29
Amiel14 <amiel1406@gmail.com>
2026-07-06 22:32:05 +0800

    Implemented ShowDAO

--- src/application/dao/impl/ShowDAOImpl.java
+++ src/application/dao/impl/ShowDAOImpl.java
@@ -1,6 +1,11 @@
 package application.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import application.dao.ShowDAO;
@@ -21,110 +26,523 @@ public class ShowDAOImpl implements ShowDAO {
 
 	@Override
 	public int addShow(Show show) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		int showId = -1;
+		String sql = "INSERT INTO shows (title, status, user_rating, num_of_seasons, num_of_episodes, avg_mins_per_ep, first_year_aired, last_year_aired) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		try (PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+		    stmt.setString(1, show.getTitle());
+		    stmt.setString(2, show.getStatus().toDbString());
+		    stmt.setDouble(3, show.getUserRating());
+		    stmt.setInt(4, show.getNumOfSeasons());
+		    stmt.setInt(5, show.getNumOfEpisodes());
+		    stmt.setInt(6, show.getAvgMinsPerEp());
+		    stmt.setInt(7, show.getFirstYearAired());
+		    stmt.setInt(8, show.getLastYearAired());
+		    stmt.executeUpdate();
+		    
+			ResultSet keys = stmt.getGeneratedKeys();
+	        if (keys.next()) {
+	            showId = keys.getInt(1);
+	        }
+	        System.out.println("Show added successfully.");
+		} catch (SQLException e) {
+			if (e.getMessage().contains("UNIQUE constraint failed")) {
+		        System.out.println("Show '" + show.getTitle() + "' is already added.");
+		    } else {
+		        System.out.println(e.getMessage());
+		    }
+		}
+		
+		if (showId != -1) {
+			sql = "INSERT OR IGNORE INTO shows_playlist_items (playlist_id, show_id)"
+				+ " VALUES (?, ?)";
+			try (PreparedStatement stmt = conn.prepareStatement(sql)){
+				stmt.setInt(1, 1);
+				stmt.setInt(2, showId);
+				stmt.executeUpdate();
+			}
+		}
+	
+		return showId;
 	}
 
 	@Override
 	public int addSeason(Season season) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		int seasonId = -1;
+		String sql = "INSERT INTO seasons (title, status) VALUES (?, ?)";
+		
+		try (PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+			stmt.setString(1, season.getTitle());
+			stmt.setString(2, season.getStatus().toDbString());
+			stmt.executeUpdate();
+			
+			ResultSet keys = stmt.getGeneratedKeys();
+			if (keys.next()) {
+				seasonId = keys.getInt(1);
+			}
+			System.out.println("Season added successfully.");
+		} catch (SQLException e) {
+			if (e.getMessage().contains("UNIQUE constraint failed")) {
+				System.out.println("Season '" + season.title + "' is already added.");
+			} else {
+				System.out.println(e.getMessage());
+			}
+		}
+		
+		return seasonId;
 	}
 
 	@Override
 	public int addEpisode(Episode episode) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		int episodeId = -1;
+		String sql = "INSERT INTO episodes (title, status, user_rating, review) VALUES (?, ?, ?, ?)";
+		
+		try (PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+			stmt.setString(1, episode.getTitle());
+			stmt.setString(2, episode.getStatus().toDbString());
+			stmt.setDouble(3, episode.getUserRating());
+			stmt.setString(4, episode.getReview());
+			stmt.executeUpdate();
+			
+			ResultSet keys = stmt.getGeneratedKeys();
+			if (keys.next()) {
+				episodeId = keys.getInt(1);
+			}
+			System.out.println("Episode added successfully.");
+		} catch (SQLException e) {
+			if (e.getMessage().contains("UNIQUE constraint failed")) {
+				System.out.println("Episode '" + episode.getTitle() + "' is already added.");
+			} else {
+				System.out.println(e.getMessage());
+			}
+		}
+		
+		return episodeId;
 	}
 
 	@Override
 	public Show getShowById(int id) throws SQLException {
-		// TODO Auto-generated method stub
+		String sql = """
+				SELECT s.id, s.title, s.status, s.user_rating, s.creator, s.avg_runtime_mins
+				FROM shows_playlists sp
+				INNER JOIN shows_playlist_items spi
+				ON sp.id = spi.playlist_id
+				INNER JOIN shows s
+				ON spi.show_id = s.id
+				WHERE sp.user_id = ? AND s.id = ?
+				""";
+		
+		try (PreparedStatement stmt = conn.prepareStatement(sql)){
+			stmt.setInt(1, userId);
+			stmt.setInt(2, id);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				return new Show(title,
+		    					Status.fromDbString(rs.getString("status")),
+		    					rs.getDouble("user_rating"),
+		    					rs.getInt("number_of_seasons"),
+		    					rs.getInt("num_of_episodes"),
+		    					rs.getInt("avg_mins_per_ep"),
+		    					rs.getInt("first_year_aired"),
+		    					rs.getInt("last_year_aired"));
+			}
+			else {
+				System.out.println("Show not found");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
 		return null;
 	}
 
 	@Override
-	public Show getShowByTitle(String title) throws SQLException {
-		// TODO Auto-generated method stub
+	public Show getShowByTitle(String title) throws SQLException {		
+		String sql = """
+		        SELECT s.id, s.title, s.status, s.user_rating, s.creator, s.avg_runtime_mins
+		        FROM shows_playlists sp
+		        JOIN shows_playlist_items spi ON sp.id = spi.playlist_id
+		        JOIN shows s ON spi.show_id = s.id
+		        WHERE s.title = ? AND sp.user_id = ?
+		    """;
+
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, title);
+		    stmt.setInt(2, userId);
+		    ResultSet rs = stmt.executeQuery();
+		    if (rs.next()) {
+		    	return new Show(title,
+		    					Status.fromDbString(rs.getString("status")),
+		    					rs.getDouble("user_rating"),
+		    					rs.getInt("number_of_seasons"),
+		    					rs.getInt("num_of_episodes"),
+		    					rs.getInt("avg_mins_per_ep"),
+		    					rs.getInt("first_year_aired"),
+		    					rs.getInt("last_year_aired"));
+		    } else {
+		    	System.out.println("Show not found.");
+		    }
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
 		return null;
 	}
 
 	@Override
 	public List<Show> getShowsByUser(int userId) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Show> shows = new ArrayList<>();
+
+		String sql = """
+				SELECT s.id, s.title, s.status, s.user_rating, s.creator, s.avg_runtime_mins
+				FROM shows_playlists sp
+				INNER JOIN shows_playlist_items spi
+				ON sp.id = spi.playlist_id
+				INNER JOIN shows s
+				ON spi.show_id = s.id
+				WHERE sp.user_id = ?
+				""";
+
+		try (PreparedStatement stmt = conn.prepareStatement(sql)){
+			stmt.setInt(1, userId);
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Show show = new Show(rs.getString("title"),
+			    					 Status.fromDbString(rs.getString("status")),
+			    					 rs.getDouble("user_rating"),
+			    					 rs.getInt("number_of_seasons"),
+			    					 rs.getInt("num_of_episodes"),
+			    					 rs.getInt("avg_mins_per_ep"),
+			    					 rs.getInt("first_year_aired"),
+			    					 rs.getInt("last_year_aired"));
+				
+				shows.add(show);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return shows;
 	}
 
 	@Override
 	public List<Season> getSeasonsByShowId(int showId) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Season> seasons = new ArrayList<>();
+		String sql = """
+				SELECT se.id, se.title, se.status
+				FROM shows_playlists sp
+				INNER JOIN shows_playlist_items spi ON sp.id = spi.playlist_id
+				INNER JOIN shows s ON spi.show_id = s.id
+				INNER JOIN show_seasons ss ON s.id = ss.show_id
+				INNER JOIN seasons se ON ss.season_id = se.id
+				WHERE sp.user_id = ? AND s.id = ?
+				""";
+				
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, userId);
+			stmt.setInt(2, showId);
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				List<Episode> episodes = getEpisodesBySeason(showId, rs.getInt("id"));
+				
+				Season season = new Season(rs.getString("title"),
+										   Status.fromDbString(rs.getString("status")),
+										   episodes);
+				
+				seasons.add(season);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return seasons;
 	}
 
 	@Override
 	public List<Season> getSeasonsByShowTitle(String title) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Season> seasons = new ArrayList<>();
+		String sql = """
+				SELECT s.id AS show_id, se.id, se.title, se.status
+				FROM shows_playlists sp
+				INNER JOIN shows_playlist_items spi ON sp.id = spi.playlist_id
+				INNER JOIN shows s ON spi.show_id = s.id
+				INNER JOIN show_seasons ss ON s.id = ss.show_id
+				INNER JOIN seasons se ON ss.season_id = se.id
+				WHERE sp.user_id = ? AND s.title = ?
+				""";
+				
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, userId);
+			stmt.setString(2, title);
+			
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				List<Episode> episodes = getEpisodesBySeason(rs.getInt("show_id"), rs.getInt("id"));
+				
+				Season season = new Season(rs.getString("title"),
+										   Status.fromDbString(rs.getString("status")),
+										   episodes);
+				seasons.add(season);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return seasons;
 	}
 
 	@Override
 	public Episode getEpisodeById(int id) throws SQLException {
-		// TODO Auto-generated method stub
+		String sql = """
+				SELECT e.title, e.status, e.user_rating, e.review
+				FROM shows_playlists sp
+				INNER JOIN shows_playlist_items spi ON sp.id = spi.playlist_id
+				INNER JOIN shows s ON spi.show_id = s.id
+				INNER JOIN show_seasons ss ON s.id = ss.show_id
+				INNER JOIN seasons se ON ss.season_id = se.id
+				INNER JOIN season_episodes sepi ON se.id = sepi.season_id
+				INNER JOIN episodes e ON sepi.episode_id = e.id
+				WHERE sp.user_id = ? AND e.id = ?
+				""";
+				
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, userId);
+			stmt.setInt(2, id);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				return new Episode(rs.getString("title"),
+								   Status.fromDbString(rs.getString("status")),
+								   rs.getDouble("user_rating"),
+								   rs.getString("review"));
+			} else {
+				System.out.println("Episode not found");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		return null;
 	}
 
 	@Override
 	public Episode getEpisodeByTitle(String title) throws SQLException {
-		// TODO Auto-generated method stub
+		String sql = """
+				SELECT e.title, e.status, e.user_rating, e.review
+				FROM shows_playlists sp
+				INNER JOIN shows_playlist_items spi ON sp.id = spi.playlist_id
+				INNER JOIN shows s ON spi.show_id = s.id
+				INNER JOIN show_seasons ss ON s.id = ss.show_id
+				INNER JOIN seasons se ON ss.season_id = se.id
+				INNER JOIN season_episodes sepi ON se.id = sepi.season_id
+				INNER JOIN episodes e ON sepi.episode_id = e.id
+				WHERE sp.user_id = ? AND e.title = ?
+				""";
+				
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, userId);
+			stmt.setString(2, title);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				return new Episode(rs.getString("title"),
+								   Status.fromDbString(rs.getString("status")),
+								   rs.getDouble("user_rating"),
+								   rs.getString("review"));
+			} else {
+				System.out.println("Episode not found.");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		return null;
+	}
+	
+	@Override
+	public List<Episode> getEpisodesBySeason(int showId, int seasonId) throws SQLException {
+		List<Episode> items = new ArrayList<Episode>();
+		
+		String sql = """
+			SELECT e.id, e.title, e.status, e.user_rating, e.review
+			FROM shows_playlists sp
+			JOIN shows_playlist_items spi ON sp.id = spi.playlist_id
+			JOIN shows s ON spi.show_id = s.id
+			JOIN show_seasons ss ON s.id = ss.show_id
+			JOIN seasons se ON ss.season_id = se.id
+			JOIN season_episodes sepi ON se.id = sepi.season_id
+			JOIN episodes e ON sepi.episode_id = e.id
+			WHERE sp.user_id = ? AND s.id = ? AND se.id = ?
+			""";
+		
+		try (PreparedStatement stmt = conn.prepareStatement(sql)){
+			stmt.setInt(1, userId);
+			stmt.setInt(2, showId);
+			stmt.setInt(3, seasonId);
+			
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Episode episode = new Episode(rs.getString("title"),
+											 Status.fromDbString(rs.getString("status")),
+											 rs.getDouble("user_rating"),
+											 rs.getString("review"));
+				
+				items.add(episode);
+			}
+		}
+		
+		return items;
 	}
 
 	@Override
 	public void updateShowStatus(String title, String status) throws SQLException {
-		// TODO Auto-generated method stub
-
+		String sql = """
+				UPDATE shows 
+				SET status = ? 
+				WHERE title = ?
+				""";
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, status);
+			stmt.setString(2, title);
+			stmt.executeUpdate();
+			System.out.println("Show updated: " + title);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	@Override
 	public void updateShowRating(String title, double rating) throws SQLException {
-		// TODO Auto-generated method stub
-
+		String sql = """
+				UPDATE shows 
+				SET user_rating = ? 
+				WHERE title = ?
+				""";
+		
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setDouble(1, rating);
+			stmt.setString(2, title);
+			stmt.executeUpdate();
+			System.out.println("Show updated: " + title);
+		}
 	}
 
 	@Override
 	public void updateEpisodeStatus(String title, String status) throws SQLException {
-		// TODO Auto-generated method stub
-
+		String sql = """
+				UPDATE episodes 
+				SET status = ? 
+				WHERE title = ?
+				""";
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, status);
+			stmt.setString(2, title);
+			stmt.executeUpdate();
+			System.out.println("Episode updated: " + title);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	@Override
 	public void updateEpisodeRating(String title, double rating) throws SQLException {
-		// TODO Auto-generated method stub
-
+		String sql = """
+				UPDATE episodes 
+				SET user_rating = ? 
+				WHERE title = ?
+				""";
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setDouble(1, rating);
+			stmt.setString(2, title);
+			stmt.executeUpdate();
+			System.out.println("Episode updated: " + title);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	@Override
-	public void addReview(String title, String review) throws SQLException {
-		// TODO Auto-generated method stub
-
+	public void updateReview(String title, String review) throws SQLException {
+		String sql = """
+				UPDATE shows 
+				SET review = ?
+				WHERE title = ?
+				""";
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, review);
+			stmt.setString(2, title);
+			stmt.executeUpdate();
+			System.out.println("Show updated: " + title);
+		}
 	}
 
 	@Override
 	public void deleteShow(String title) throws SQLException {
-		// TODO Auto-generated method stub
-
+		String sql = "DELETE FROM shows WHERE title = ?";
+	    try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+	        stmt.setString(1, title);
+	        stmt.executeUpdate();
+	        System.out.println("Show '" + title + "' deleted");
+	    }
 	}
 
 	@Override
 	public List<Show> getShowsByStatus(Status status) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Show> shows = new ArrayList<>();
+		String sql = """
+				SELECT s.id, s.title, s.status, s.user_rating, s.creator, s.avg_runtime_mins
+				FROM shows_playlists sp
+				INNER JOIN shows_playlist_items spi ON sp.id = spi.playlist_id
+				INNER JOIN shows s ON spi.show_id = s.id
+				WHERE sp.user_id = ? AND s.status = ?
+				""";
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, userId);
+			stmt.setString(2, status.toDbString());
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Show show = new Show(rs.getString("title"),
+					   				 Status.fromDbString(rs.getString("status")),
+					   				 rs.getDouble("user_rating"),
+					   				 rs.getInt("number_of_seasons"),
+					   				 rs.getInt("num_of_episodes"),
+					   				 rs.getInt("avg_mins_per_ep"),
+					   				 rs.getInt("first_year_aired"),
+					   				 rs.getInt("last_year_aired")); 
+				
+				shows.add(show);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return shows;
 	}
 
 	@Override
 	public List<Show> getShowsByGenre(String genre) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Show> shows = new ArrayList<>();
+		String sql = """
+				SELECT s.id, s.title, s.status, s.user_rating, s.creator, s.avg_runtime_mins
+				FROM shows_playlists sp
+				INNER JOIN shows_playlist_items spi ON sp.id = spi.playlist_id
+				INNER JOIN shows s ON spi.show_id = s.id
+				INNER JOIN show_genres sg ON s.id = sg.show_id
+				INNER JOIN genres g ON sg.genre_id = g.id
+				WHERE sp.user_id = ? AND g.genre = ?
+				""";
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, userId);
+			stmt.setString(2, genre);
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Show show = new Show(rs.getString("title"),
+				   				    Status.fromDbString(rs.getString("status")),
+				   				    rs.getDouble("user_rating"),
+				   				    rs.getInt("number_of_seasons"),
+				   				    rs.getInt("num_of_episodes"),
+				   				    rs.getInt("avg_mins_per_ep"),
+				   				    rs.getInt("first_year_aired"),
+				   				    rs.getInt("last_year_aired"));
+				shows.add(show);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return shows;
 	}
-
 }
