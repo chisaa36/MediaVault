4afcb046d7570e8342f05630406e31a9fa230254
Amiel14 <amiel1406@gmail.com>
2026-07-10 01:24:24 +0800

    Merged conflicts:

--- src/application/model/SongPlaylist.java
+++ src/application/model/SongPlaylist.java
@@ -5,20 +5,24 @@ import java.util.ArrayList;
 
 public class SongPlaylist {
 	
+	private int playlistId;
 	public String title;
 	public List<Song> songs = new ArrayList<>();
 	
 	// constructor
-	public SongPlaylist(String title, List<Song> songs) {
+	public SongPlaylist(String title, List<Song> songs, int playlistId) {
 		super();
 		this.title = title;
 		this.songs = songs;
+		this.playlistId = playlistId;
 	}
 	
 	// getters and setters
 	public String getTitle() {return title;}
 	public List<Song> getSongs() {return songs;}
+	public int getPlaylistId() {return playlistId;}
 	
 	public void setTitle(String title) {this.title = title;}
 	public void setSongs(List<Song> songs) {this.songs = songs;}
+	public void setPlaylistId(int playlistId) {this.playlistId = playlistId;}
 }
